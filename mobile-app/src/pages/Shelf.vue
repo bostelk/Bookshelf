85991d8bfcd193e4cbbc022354a87a6144924948
Kyle Bostelmann <kyle@bostelk.ca>
2025-05-29 00:10:25 -0300

    favicon, copy.

--- mobile-app/src/pages/Shelf.vue
+++ mobile-app/src/pages/Shelf.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 class="text-2xl font-bold">Shelfs</h1>
-  <p class="text-gray-500">Search books in your collection</p>
+  <p class="text-gray-500">Search books in your library.</p>
   <br />
   <input type="text" placeholder="..." class="bg-gray-100 rounded" style="width: 250px" />
   &nbsp;<button class="bg-black hover:bg-white border-1 hover:text-black text-white px-2 rounded">
